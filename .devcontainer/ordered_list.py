37243ba955b53f4dc51eb3bbfd70883d5590293d
zxdd6186-uol <zxdd6186@leeds.ac.uk>
2025-02-06 16:35:19 +0000

    code

--- .devcontainer/ordered_list.py
+++ .devcontainer/ordered_list.py
@@ -1,2 +1,6 @@
-def get_ordered_list(list):
-    if list
+def get_ordered_list(numbers=None):
+    if numbers is None:
+        user_input = input("Enter a list of integers (comma-separated): ")
+        numbers = [int(num.strip()) for num in user_input.split(",")]
+
+    return sorted(numbers)
